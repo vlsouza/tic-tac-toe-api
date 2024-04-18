e8a995cac86e5fbef1f5961debb746840e864342
Vinicius Souza <viniciuslima76@outlook.com.br>
2024-04-18 20:29:14 +0100

    connect to dynamodb

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"main/match"
 	"net/http"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	initDBClient()
 	initAPI()
 }
 
@@ -29,4 +33,30 @@ func initAPI() {
 
 	port := "8080"
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
+}
+
+func initDBClient() {
+	ctx := context.Background()
+
+	// Carregando configurações padrão
+	cfg, err := config.LoadDefaultConfig(ctx,
+		config.WithRegion("eu-north-1"),
+	)
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	// Criando o cliente DynamoDB
+	svc := dynamodb.NewFromConfig(cfg)
+
+	// Chamada de exemplo: Listar tabelas
+	result, err := svc.ListTables(ctx, &dynamodb.ListTablesInput{})
+	if err != nil {
+		log.Fatalf("failed to list tables, %v", err)
+	}
+
+	fmt.Println("Tables:")
+	for _, tableName := range result.TableNames {
+		fmt.Println(tableName)
+	}
 }
