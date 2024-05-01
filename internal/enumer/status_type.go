1480561d1e7ac7663170f7d9f1ab34f7f4b0b281
Vinicius Souza <viniciuslima76@outlook.com.br>
2024-05-01 14:31:17 +0100

    add marshal and unmarshal methods for dynamodb for status type

--- internal/enumer/status_type.go
+++ internal/enumer/status_type.go
@@ -1,5 +1,11 @@
 package enumer
 
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
 //go:generate enumer -json -text -sql -type StatusType -trimprefix Status -transform snake-upper
 
 // Status represents the current status of a game
@@ -12,3 +18,17 @@ const (
 	PLAYER2WON
 	DRAW
 )
+
+func (s StatusType) ToAttributeValue() types.AttributeValue {
+	return &types.AttributeValueMemberS{Value: s.String()}
+}
+
+func (s *StatusType) FromAttributeValue(av types.AttributeValue) error {
+	value, ok := av.(*types.AttributeValueMemberS)
+	if !ok {
+		return fmt.Errorf("expected string value for StatusType")
+	}
+	var err error
+	*s, err = StatusTypeString(value.Value)
+	return err
+}
