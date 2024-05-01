ec94a553d0033bf254c0e0bebead66bc62f291f6
Vinicius Souza <viniciuslima76@outlook.com.br>
2024-05-01 15:44:31 +0100

    handle enums get method

--- internal/enumer/player_type.go
+++ internal/enumer/player_type.go
@@ -1,5 +1,11 @@
 package enumer
 
+import (
+	"errors"
+
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
 //go:generate enumer -json -text -sql -type PlayerType -trimprefix Player -transform snake-upper
 
 // Status represents the available players in a match
@@ -9,3 +15,18 @@ const (
 	PLAYER1 PlayerType = iota
 	PLAYER2
 )
+
+func (p PlayerType) MarshalDynamoDBAttributeValue() (types.AttributeValue, error) {
+	s := p.String() // Ensure that String method returns the correct string representation
+	return &types.AttributeValueMemberS{Value: s}, nil
+}
+
+func (p *PlayerType) UnmarshalDynamoDBAttributeValue(av types.AttributeValue) error {
+	// Type assert to *types.AttributeValueMemberS to handle string values
+	if s, ok := av.(*types.AttributeValueMemberS); ok {
+		var err error
+		*p, err = PlayerTypeString(s.Value)
+		return err
+	}
+	return errors.New("expected string value for PlayerType")
+}
